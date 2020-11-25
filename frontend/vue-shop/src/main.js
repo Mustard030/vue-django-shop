001794d6e9a3e73927a14ced5b733dc13fd9ae45
jiemoo <978090944@qq.com>
2020-11-26 02:24:40 +0800

    staticfiles

--- frontend/vue-shop/src/main.js
+++ frontend/vue-shop/src/main.js
@@ -2,7 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'element-ui'
 import './plugins/element.js'
+import './assets/css/global.css'
+import axios from 'axios'
+
+// 配置请求根路径
+// axios.defaults.baseURL = ''
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
