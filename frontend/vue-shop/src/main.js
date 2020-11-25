141a96d4362a74a19b974abb37c57292e539ce4d
jiemoo <978090944@qq.com>
2020-11-26 03:20:36 +0800

    change port

--- frontend/vue-shop/src/main.js
+++ frontend/vue-shop/src/main.js
@@ -8,7 +8,7 @@ import './assets/css/global.css'
 import axios from 'axios'
 
 // 配置请求根路径
-axios.defaults.baseURL = 'http://localhost:9900/api/private/'
+axios.defaults.baseURL = 'http://localhost:80/api/private/'
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
