864e0bb86a233c29360d391f00ca5500c7bc07eb
jiemoo <978090944@qq.com>
2020-11-26 04:15:11 +0800

    config

--- frontend/vue-shop/vue.config.js
+++ frontend/vue-shop/vue.config.js
@@ -1,13 +1,6 @@
 
 module.exports = {
-    // 项目部署的基础路径
-    // 我们默认假设你的应用将会部署在域名的根部，
-    // 比如 https://www.my-app.com/
-    // 如果你的应用时部署在一个子路径下，那么你需要在这里
-    // 指定子路径。比如，如果你的应用部署在
-    // https://www.foobar.com/my-app/
-    // 那么将这个值改为 `/my-app/`
-    baseUrl: '/',
+    
  
     // 将构建好的文件输出到哪里
     outputDir: 'dist',
